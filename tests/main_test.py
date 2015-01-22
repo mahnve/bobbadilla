02f7c191f47822e1edc9b4d6f9b2c363c310adc6
Marcus Ahnve <marcus@ahnve.com>
2015-01-22 14:03:35 +0100

    wip

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,18 +1,16 @@
 import unittest
-import os
-import sys
 import data_builder
 from nose.tools import assert_equal
 
 sys.path.append(os.path.abspath(".."))
 
-from bobbadilla import file_reader
+from bobbadilla import main
 
 
-class ReadFileTest(unittest.TestCase):
-    def test_read_size_of_a_1_mb_file(self):
-        data_builder.FileBuilder().with_path(
-            'build/test_data/1_mb_file').with_size(1).build()
+class BobbadillaTest(unittest.TestCase):
+    def test_empty_m3u(self):
+        m3u = data_builder.m3uBuilder().build()
+        output = data_builder.
 
         the_file = file_reader.FileReader().read('build/test_data/1_mb_file')
         assert_equal(the_file['size'], 1)
